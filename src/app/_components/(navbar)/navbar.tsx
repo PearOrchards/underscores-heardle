d730c43dcb9a5e23f0d2d72b4e5d36b66ab315c8
PearOrchards <pkedzia88@gmail.com>
2024-04-06 21:09:16 +0100

    Added second dialog

--- src/app/_components/(navbar)/navbar.tsx
+++ src/app/_components/(navbar)/navbar.tsx
@@ -2,7 +2,8 @@
 import styles from "./navbar.module.scss";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleInfo, faChartSimple, faCircleQuestion } from "@fortawesome/free-solid-svg-icons";
+import { faCircleInfo, faChartSimple, faCircleQuestion, faMusic, faVolumeHigh, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+
 
 import { useState } from "react";
 import Link from "next/link";
@@ -11,12 +12,13 @@ import Dialog from "@/app/_components/(dialog)/dialog";
 
 export default function Navbar() {
 	const [showAboutModal, setShowAboutModal] = useState<boolean>(false);
-	const openAboutModal = () => {
-		setShowAboutModal(true);
-	}
-	const closeAboutModal = () => {
-		setShowAboutModal(false);
-	}
+	const [showChartModal, setShowChartModal] = useState<boolean>(false);
+	const [showHelpModal, setShowHelpModal] = useState<boolean>(false);
+	const openAboutModal = () => setShowAboutModal(true);
+	const closeAboutModal = () => setShowAboutModal(false);
+	
+	const openHelpModal = () => setShowHelpModal(true);
+	const closeHelpModal = () => setShowHelpModal(false);
 	
 	return (
 		<nav className={styles.nav}>
@@ -26,6 +28,7 @@ export default function Navbar() {
 					<h2>about</h2>
 					<p>A recreation of Heardle for underscores, with all songs taken from the tracker (beyond skin purifying treatment).</p>
 					<p>Built by <Link href="https://www.orchards.dev">Pear</Link>, for the <Link href="https://www.youtube.com/playlist?list=PLAEc0HKmIWO-oDNPLi1aL1aazsqdxYhPg">daily underscores heardle series</Link>.</p>
+					<p>Also please check out the <Link href="https://heardle.apictureof.me/">original underscores heardle</Link> by shelly, it contains only soundcloud releases for those who want a fairer chance</p>
 				</Dialog>
 			</div>
 			<div className={styles.center}>
@@ -33,7 +36,21 @@ export default function Navbar() {
 			</div>
 			<div className={styles.right}>
 				<FontAwesomeIcon icon={faChartSimple} className={styles.fa} />
-				<FontAwesomeIcon icon={faCircleQuestion} className={styles.fa} />
+				
+				
+				<FontAwesomeIcon icon={faCircleQuestion} className={styles.fa} onClick={openHelpModal} />
+				<Dialog isOpen={showHelpModal} onClose={closeHelpModal}>
+					<h2>how to play</h2>
+					<div>
+						<p><FontAwesomeIcon icon={faMusic} /> Listen to the intro, then find the correct song in the list.</p>
+						<p><FontAwesomeIcon icon={faVolumeHigh} /> Skipped or incorrect attempts unlock more of the intro.</p>
+						<p><FontAwesomeIcon icon={faThumbsUp} /> Answer in as few tries as possible and share your score!</p>
+					</div>
+					<h3><i>good luck!</i></h3>
+					
+					<button className="alt" onClick={closeHelpModal}>PLAY</button>
+					
+				</Dialog>
 			</div>
 		</nav>
 	)
