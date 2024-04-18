c6d4bc5e993339d98797b81c8bc2b77656f4e10e
PearOrchards <pkedzia88@gmail.com>
2024-04-18 17:23:22 +0100

    Added stats

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,12 @@ import "./globals.css";
 
 const rethinkSans = Rethink_Sans({ subsets: ["latin"] });
 
+// Import to stop flash of very large icon.
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Now stop fontawesome from doing that itself.
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
+
 export const metadata: Metadata = {
   title: "underscores heardle",
   description: "Made by PearOrchards, inspired by heardle.apictureof.me",
