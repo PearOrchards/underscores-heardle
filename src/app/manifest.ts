4afec86e4631b9cd1382503ba4477600403f75ac
PearOrchards <pkedzia88@gmail.com>
2024-04-24 14:46:24 +0100

    Added favicons... I think

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+	return {
+		name: 'underscores heardle',
+		short_name: "underscores heardle",
+		description: "A clone of Heardle for underscores songs, by orchards.dev",
+		start_url: '/',
+		display: 'standalone',
+		background_color: '#222222',
+		theme_color: '#222222',
+		icons: [
+			{
+				"src": "/images/icon-192.png",
+				"sizes": "192x192",
+				"type": "image/png"
+			},
+			{
+				"src": "/images/icon-512.png",
+				"sizes": "512x512",
+				"type": "image/png"
+			},
+		],
+	}
+}
