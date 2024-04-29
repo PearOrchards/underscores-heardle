020bda718bc9272f2573cc63fbf7cbf3f9097e60
PearOrchards <pkedzia88@gmail.com>
2024-04-29 18:42:43 +0100

    Further mobile fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,31 @@
-import type { Metadata } from "next";
-import { Rethink_Sans } from "next/font/google";
+import type {Metadata, Viewport} from "next";
+import {Rethink_Sans} from "next/font/google";
 import "./globals.css";
 
-const rethinkSans = Rethink_Sans({ subsets: ["latin"] });
+const rethinkSans = Rethink_Sans({subsets: ["latin"]});
 
 // Import to stop flash of very large icon.
 import '@fortawesome/fontawesome-svg-core/styles.css';
 // Now stop fontawesome from doing that itself.
-import { config } from '@fortawesome/fontawesome-svg-core';
+import {config} from '@fortawesome/fontawesome-svg-core';
+
 config.autoAddCss = false;
 
 export const metadata: Metadata = {
-  title: "underscores heardle",
-  description: "Made by PearOrchards, inspired by heardle.apictureof.me",
+	title: "underscores heardle",
+	description: "Made by PearOrchards, inspired by heardle.apictureof.me",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={rethinkSans.className}>{children}</body>
-    </html>
-  );
+export const viewport: Viewport = {
+	themeColor: "#222",
+}
+
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
+	return (
+		<html lang="en">
+			<body className={rethinkSans.className}>
+				{children}
+			</body>
+		</html>
+	);
 }
