bac6fa682584d6f5a334688629f1956619e3c333
PearOrchards <pkedzia88@gmail.com>
2024-06-08 13:01:16 +0100

    Fixed vanishing suggestions
    This was a bug where the suggestions would just disappear if mouse button is held too long, and not autocomplete

--- src/app/_components/(game)/(input)/input.tsx
+++ src/app/_components/(game)/(input)/input.tsx
@@ -96,7 +96,7 @@ export default function Input({ currentAttempt, complete, guess, skip } : { curr
 			<form className={styles.input} onSubmit={async (ev) => { ev.preventDefault(); await submitGuess() }}>
 				<div className={`${styles.suggestions} ${showSuggestions ? styles.suggestionsShow : ""}`} ref={suggestionsRef}>
 					{ suggestions.slice(0, 9).map(s => (
-						<p onMouseEnter={hover} onClick={() => autocomplete(s)} key={s}>{s}</p>
+						<p onMouseEnter={hover} onMouseDown={() => autocomplete(s)} key={s}>{s}</p>
 					)) }
 				</div>
 				<input type="text" onFocus={onFocus} onBlur={blur} onChange={onFocus} onKeyDown={keyPress} placeholder="Know it? Search for the title" ref={inputRef} data-cy="textInput" />
