248d1777cbf5a9731658026a9eaa4303767fae63
PearOrchards <pkedzia88@gmail.com>
2025-03-29 12:16:29 +0100

    feat: added soundcloud auth
    might not be necessary but /shrug

--- src/app/api/audio/route.ts
+++ src/app/api/audio/route.ts
@@ -1,20 +1,26 @@
 import { SongToday } from "@/app/_components/SongToday";
+import { getSoundcloudToken, createAccess } from "@/app/_components/AuthSoundcloud";
 
 async function getSoundcloudAudio(url: string): Promise<string> {
 	const songData = await fetch(`https://api-widget.soundcloud.com/resolve?url=${url}&client_id=${process.env.SOUNDCLOUD_CLIENT}&format=json`)
 	if (!songData.ok) return ""; // Likely 401, update the .env file with a new client ID
 	const songJson = await songData.json();
-	
+
 	if (!songData.ok) return "";
 	if (songJson.media.transcodings[1].format.protocol !== "progressive") return ""; // :(
 	const newURL = songJson.media.transcodings[1].url;
-	
+
 	const trackData = await fetch(newURL + `?client_id=${process.env.SOUNDCLOUD_CLIENT}`);
 	const trackJson = await trackData.json();
-	
+
 	if (!trackData.ok || !trackJson.url) return "";
 	return trackJson.url;
 }
+
+// async function getSoundcloudAudioV2(url: string): Promise<string> {
+// 	// const access = await getSoundcloudToken();
+// 	const songData = await fetch(`https://api.soundcloud.com/tracks/${}`)
+// }
 
 async function getPillowcaseAudio(url: string): Promise<string> {
 	const id = url.split("/").pop();
@@ -24,7 +30,7 @@ async function getPillowcaseAudio(url: string): Promise<string> {
 export async function GET() {
 	const { link, source, offset } = await SongToday();
 	let audioFile = "";
-	
+
 	switch (source) {
 		case "soundcloud":
 			audioFile = await getSoundcloudAudio(link);
@@ -37,13 +43,13 @@ export async function GET() {
 				status: 500,
 			});
 	}
-	
+
 	try {
 		const audio = await fetch(audioFile, {
 			signal: AbortSignal.timeout(5000) // Give up after 5 seconds
 		});
 		const audioBuffer = await audio.arrayBuffer();
-		
+
 		return new Response(audioBuffer, {
 			headers: {
 				"Content-Type": "audio/mpeg",
