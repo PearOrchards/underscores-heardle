37483478f2bb86bd82ff7d2ef054c6d866621f91
Pawel Kedzia <pkedzia88@gmail.com>
2026-05-31 12:06:50 +0200

    chore: migrate to newer versions

--- src/app/_components/(navbar)/navbar.tsx
+++ src/app/_components/(navbar)/navbar.tsx
@@ -6,24 +6,26 @@ import About from "./parts/about";
 import Login from "./parts/login";
 import Logout from "./parts/logout";
 
-import { auth } from "@/auth";
+import { useSession } from "next-auth/react";
 
-export default async function Navbar({ display }: { display?: string }) {
-    const session = await auth();
+export default function Navbar({ display }: { display?: string }) {
+  const { data: session, status } = useSession();
 
-	return (
-		<nav className={styles.nav}>
-			<div className={styles.left}>
-				<About />
-                <Help />
-			</div>
-			<div className={styles.center}>
-				<h1 data-cy='page-title'>{ display ? `${display} heardle` : `heardle`}</h1>
-			</div>
-			<div className={styles.right}>
-				<Stats />
-                { !session?.user ? ( <Login /> ) : ( <Logout />) }
-			</div>
-		</nav>
-	)
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.left}>
+        <About />
+        <Help />
+      </div>
+      <div className={styles.center}>
+        <h1 data-cy="page-title">
+          {display ? `${display} heardle` : `heardle`}
+        </h1>
+      </div>
+      <div className={styles.right}>
+        <Stats />
+        {!session?.user ? <Login /> : <Logout />}
+      </div>
+    </nav>
+  );
 }
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,17 @@
-import { handlers } from "@/auth";
-export const { GET, POST } = handlers;
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+});
+
+export { handler as GET, handler as POST };
